7a2a19ec8562536216c86928dc42d7c8facff575
Abtahi Hasan <abtahihasan7472@gmail.com>
2026-09-13 11:37:15 +0600

    :art: user and manager dashbaord ui added

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
-# def show_tasks():
-#     return render()
+def manager_dashboard(request):
+    return render(request, "manager-dashboard.html")
+
+
+def user_dashboard(request):
+    return render(request, "user-dashboard.html")
